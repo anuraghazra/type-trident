bfbb0d988d0a13e51ce1bf466486035eeb720466
Anurag <hazru.anurag@gmail.com>
2022-05-25 21:43:46 +0530

    chore: add fun fact about perf for ds-tokenizer

--- src/ds-tokenizer/index.ts
+++ src/ds-tokenizer/index.ts
@@ -1,3 +1,16 @@
+/**
+ * Fun fact: 
+ * 
+ * In current version of TypeScript 4.6 
+ * This TokenizeTheme<> type is slow to compute but there is a faster version
+ * While exploring this, I found this performance optimization trick
+ * More details in this tweet: https://twitter.com/anuraghazru/status/1501989501060583443
+ * 
+ * And this performance bug will be fixed in 4.8.1 
+ * https://github.com/microsoft/TypeScript/issues/49136
+ * Thanks to Andarist for opening this issue. 
+ */
+
 type Dollar<First extends boolean> = First extends true ? "$" : "";
 
 type TokenizeTheme<Theme, First extends boolean = true> = {
